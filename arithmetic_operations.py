7d01d5a77dac198888aaef4f80fd91d2e2b9f801
MrGranai <arturgranaif@gmail.com>
2024-05-22 19:40:45 -0300

    Add simple arithmetic operations script

---
+++ arithmetic_operations.py
@@ -0,0 +1,13 @@
+# Simple arithmetic operations
+
+# Addition
+result = 6 + 23 + 56
+print("The result of addition is:", result)
+
+# Subtraction and multiplication
+result = (87 - 9) * 4
+print("The result of subtraction and multiplication is:", result)
+
+# More complex arithmetic involving subtraction and division
+result = (9 - (1 + 2)) / 3.0
+print("The result of this complex operation is:", result)
